df5f33e60391e5258e0816999829b62a2191683e
ominachan69 <129273629+ominachan69@users.noreply.github.com>
2024-05-07 05:51:36 +0700

    Add files via upload

---
+++ kernel.sh
@@ -0,0 +1,3 @@
+git config --global user.email arifbuditantodablekk@gmail.com
+git config --global user.name ariffjenong
+git clone https://ariffjenong:$GH_TOKEN@github.com/ariffjenong/android_kernel_sony_msm8998 -b lineage-19.0_bpf_backport_new
